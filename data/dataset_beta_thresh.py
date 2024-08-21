93c037e7cc0ff37a00ab3e3ecec9a633791065c4
ThanhMTA <96895318+ThanhMTA@users.noreply.github.com>
2024-08-21 14:25:22 +0700

    Update dataset_beta_thresh.py

--- data/dataset_beta_thresh.py
+++ data/dataset_beta_thresh.py
@@ -134,7 +134,7 @@ class MVTecTrainDataset(Dataset):
         self.resize_shape = [img_size[0], img_size[1]]
         self.anomaly_source_path = args["anomaly_source_path"]
         self.image_paths = sorted(glob.glob(self.root_dir+"/*.png"))
-        self.anomaly_source_paths = sorted(glob.glob(self.anomaly_source_path+"/good/*.png"))
+        self.anomaly_source_paths = sorted(glob.glob(self.anomaly_source_path+"/*.png"))
 
         self.augmenters = [iaa.GammaContrast((0.5, 2.0), per_channel=True),
                            iaa.MultiplyAndAddToBrightness(
